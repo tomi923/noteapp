fd6e7cddde4e2a6bd202ee359ed7a4c72d111e18
tomi923 <tomaszwisniewski055@gmail.com>
2026-02-03 00:20:30 +0100

    Add PWA icon support for Android and Windows

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 const CACHE_NAME = "noteapp-static-v1";
-const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.json"];
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.json",
+  "/icon.svg",
+  "/icon-maskable.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
